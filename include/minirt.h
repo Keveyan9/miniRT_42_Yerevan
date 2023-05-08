a75c6ccbcde8f7aff9fef2d1a3340aab9f73c7a5
Sargis Keveyan <keveyansargis@gmail.com>
2023-05-08 19:04:48 +0400

    first_merge

---
+++ include/minirt.h
@@ -0,0 +1,66 @@
+#ifndef MINIRT_H
+# define MINIRT_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include "../libft/libft.h"
+# include "../get_next_line/get_next_line.h"
+# define SPLIT_SET " \t\v\b"
+
+typedef struct s_vec
+{
+	float x;
+	float y;
+	float z;
+}	t_vec;
+
+typedef struct s_color
+{
+	float r;
+	float g;
+	float b;
+}	t_color;
+
+typedef struct s_cam
+{
+	t_vec *orig;
+	t_vec *orientation;
+	float	fov;
+}	t_cam;
+
+typedef struct s_light
+{
+	t_vec	*orig;
+	float	ratio;
+} t_light;
+
+typedef struct s_sphere
+{
+	t_vec *center;
+	t_color *tint;
+	float radius;
+} t_sphere;
+
+typedef struct s_plane
+{
+	t_vec *point;
+	t_vec *normal;
+	t_color *tint;
+} t_plane;
+
+typedef struct s_cylinder
+{
+	t_vec *center;
+	t_vec *normal;
+	float radius;
+	float height;
+	t_color *tint;
+} t_cylinder;
+
+int 	is_in_set(char c);
+char	**ft_split_m(char const *s);
+void    exit_code(int code, char *msg);
+int		ft_double_len(char **str);
+float 	ft_atof(char *str);
+#endif
---
+++ src/init.c
@@ -0,0 +1,93 @@
+#include "../includes/minirt.h"
+
+t_vec	*init_vector(float x, float y, float z)
+{
+	t_vec *vector;
+
+	vector = malloc(sizeof(t_vec));
+	if (!vector)
+		exit_code(1, "vector malloc failed");
+	vector->x = x;
+	vector->y = y;
+	vector->z = z;
+	return (vector);
+}
+
+t_color	*init_color(float r, float g, float b)
+{
+	t_color *color;
+
+	color = malloc(sizeof(t_color));
+	if (!color)
+		exit_code(1, "color malloc failed");
+	color->r = r;
+	color->g = g;
+	color->b = b;
+	return (color);
+}
+
+t_cam	*init_cam(t_vec *orient, t_vec *orig, float fov)
+{
+	t_cam *camera;
+
+	camera = malloc(sizeof(t_cam));
+	if (!camera)
+		exit_code(1, "camera malloc failed");
+	camera->orientation = orient;
+	camera->orig = orig;
+	camera->fov = fov;
+	return (camera);
+}
+
+t_light	*init_light(t_vec *orig, float ratio)
+{
+	t_light *light;
+
+	light = malloc(sizeof(t_light));
+	if (!light)
+		exit_code(1, "origin malloc failed");
+	light->orig = orig;
+	light->ratio = ratio;
+	return (light);
+}
+
+t_sphere	*init_sphere(t_vec *center, t_color *tint, float radius)
+{
+	t_sphere *sphere;
+
+	sphere = malloc(sizeof(t_sphere));
+	if (!sphere)
+		exit_code(1, "sphere malloc failed");
+	sphere->center = center;
+	sphere->tint = tint;
+	sphere->radius = radius;
+	return (sphere);
+}
+
+t_plane	*init_plane(t_vec *point, t_vec *normal, t_color *tint)
+{
+	t_plane *plane;
+
+	plane = malloc(sizeof(t_plane));
+	if (!plane)
+		exit_code(1, "plane malloc failed");
+	plane->point = point;
+	plane->normal = normal;
+	plane->tint = tint;
+	return (plane);
+}
+
+t_cylinder	*init_cylinder(t_vec *center, t_vec *normal, float radius, float height, t_color *tint)
+{
+	t_cylinder *cylinder;
+
+	cylinder = malloc(sizeof(t_cylinder));
+	if (!cylinder)
+		exit_code(1, "cylinder malloc failed");
+	cylinder->center = center;
+	cylinder->normal = normal;
+	cylinder->radius = radius;
+	cylinder->height = height;
+	cylinder->tint = tint;
+	return (cylinder);
+}

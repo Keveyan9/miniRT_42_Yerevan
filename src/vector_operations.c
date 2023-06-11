659e3268f97fd44a884e88b57aceda76f1962af6
AniIvanyan <aniivanyan23@gmail.com>
2023-06-12 02:52:24 +0400

    vector operations

--- src/vector_operations.c
+++ src/vector_operations.c
@@ -6,48 +6,70 @@
 /*   By: aivanyan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 16:20:59 by aivanyan          #+#    #+#             */
-/*   Updated: 2023/06/10 15:32:59 by aivanyan         ###   ########.fr       */
+/*   Updated: 2023/06/12 02:51:07 by aivanyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-void vec_subtract(float c[3], float a[3], float b[3])
+t_vec   vec_add(t_vec a, t_vec b)
 {
-    int i;
+	t_vec	p;
 
-    i = -1;
-    while (++i < 3)
-        c[i] = a[i] - b[i];
+	p.x = a.x + b.x;
+	p.y = a.y + b.y;
+	p.z = a.z + b.z;
+	return (p);
 }
 
-void vec_add(float c[3], float a[3], float b[3])
+t_vec   vec_sub(t_vec a, t_vec b)
 {
-    int i;
+	t_vec	p;
 
-    i = -1;
-    while (++i < 3)
-        c[i] = a[i] + b[i];
+	p.x = a.x - b.x;
+	p.y = a.y - b.y;
+	p.z = a.z - b.z;
+	return (p);
 }
 
-float dot_product(float a[3], float b[3])
+float   dot_product(t_vec a, t_vec b)
 {
-    return (a[0] * b[0] + a[1] * b[1] + a[2] * b[2]);
+	return (a.x * b.x + a.y * b.y + a.z * b.z);
 }
 
-void cross(float c[3], float a[3], float b[3])
+t_vec   cross(t_vec a, t_vec b)
 {
-    c[0] = a[1] * b[2] - a[2] * b[1];
-    c[1] = a[2] * b[0] - a[0] * b[2];
-    c[2] = a[0] * b[1] - a[1] * b[0];
+	t_vec	cp;
+
+	cp.x = a.y * b.z - a.z * b.y;
+	cp.y = a.z * b.x - a.x * b.z;
+	cp.z = a.x * b.y - a.y * b.x;
+	return (cp);
 }
 
-void normalize(float p[3])
+t_vec   normalize(t_vec p)
 {
-    float len;
+	t_vec	n;
+	float	len;
 
-    len = sqrtf(p[0] * p[0] + p[1] * p[1] + p[2] * p[2]);
-    p[0] = p[0] / len;
-    p[1] = p[1] / len;
-    p[2] = p[2] / len;
+	len = sqrtf(p.x * p.x + p.y * p.y + p.z * p.z);
+	n.x = p.x / len;
+	n.y = p.y / len;
+	n.z = p.z / len;
+	return (n);
+}
+
+t_vec   scale_x_vec(double n, t_vec p)
+{
+	t_vec   v;
+
+	v.x = n * p.x;
+	v.y = n * p.y;
+	v.z = n * p.z;s
+	return (v);
+}
+
+float   distance(t_vec p1, t_vec p2)
+{
+	return (sqrtf(powf(p2.x - p1.x, 2) + powf(p2.y - p1.y, 2) + powf(p2.z - p1.z, 2)));
 }

e0edbf05921e196c3bb6e266fb8849ca2daa0792
Sargis Keveyan <keveyansargis@gmail.com>
2023-07-06 18:36:32 +0400

    the_same_Keveyan

--- includes/mymlx.h
+++ includes/mymlx.h
@@ -1,15 +1,18 @@
-#ifndef MYMLX_H
+#ifndef	MYMLX_H
 # define MYMLX_H
+
+# include "minirt.h"
 
 typedef struct s_mlx
 {
-    void    *win;
-    void    *mlx;
-    void    *img;
-    char    *addr;
-    int     bpp;
-    int     line_length;
-    int     endian;
-}   t_mlx;
+	void	*win;
+	void	*mlx;
+	void	*img;
+	char	*addr;
+	int		bpp;
+	int		line_length;
+	int		endian;
+}	t_mlx;
+
 
 #endif

c08ffe227ca777358cba9095d21c41e3993ecdea
Sargis Keveyan <keveyansargis@gmail.com>
2023-07-07 21:01:55 +0400

    test_comand_number

--- srcs/main.c
+++ srcs/main.c
@@ -1,5 +1,19 @@
 #include "minirt.h"
 
+ static int	clos(t_scene *scene)
+{
+	exit_code(0, "finish\n", NULL, NULL);
+}
+
+int	key_hook_man(int code, t_scene *scene)
+{
+	if (code == 53)
+		exit_code(0, "finish_esc\n", NULL, NULL);
+		printf("comand__%d\n",code);
+	return (0);
+}
+
+
 int	main(int argc, char **argv)
 {
 	t_scene	scene;
@@ -9,8 +23,9 @@ int	main(int argc, char **argv)
 	mlxInit(&mlxData);
 	parsing(argc, argv, &scene);
 	render(&scene, &mlxData);
+	mlx_key_hook(mlxData.win, key_hook_man, &scene);
+	mlx_hook(mlxData.win, 17, 0, clos, &scene);
 	free_scene(&scene);
 	mlx_loop(mlxData.mlx);
-	while (1);
 	return (0);
 }

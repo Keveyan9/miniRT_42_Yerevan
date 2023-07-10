82577581845cfd1d68a6441b67940b90c9aaa398
Sargis Keveyans <skeveyan@c2r9s2.42yerevan.am>
2023-07-10 21:14:35 +0400

    norminet

--- includes/mymlx.h
+++ includes/mymlx.h
@@ -1,21 +1,15 @@
-#ifndef MYMMlX_H
-# define MYMMlX_H
+#ifndef MYMLX_H
+# define MYMLX_H
 
 typedef struct s_mlx
 {
-	void	*win;
-	void	*mlx;
-	void	*img;
-	char	*addr;
-	int		bpp;
-	int		line_length;
-	int		endian;
-}	t_mlx;
-
-typedef struct s_all
-{
-	t_mlx	*mlx_data;
-	t_scene	*scene;
-}	t_all;
+    void    *win;
+    void    *mlx;
+    void    *img;
+    char    *addr;
+    int     bpp;
+    int     line_length;
+    int     endian;
+}   t_mlx;
 
 #endif
